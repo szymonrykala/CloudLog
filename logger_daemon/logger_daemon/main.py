9848cffc360313970a3c33f256893867f2179a41
pazderskipawel <pawelpazderski@hotmail.com>
2023-04-12 19:59:05 +0200

    fixed windows , chnaged linux output, time is var

--- logger_daemon/logger_daemon/main.py
+++ logger_daemon/logger_daemon/main.py
@@ -8,7 +8,6 @@ import platform
 import json
 from cloudlog_commons import Log, OS, LogType
 
-
 class LogWorker(threading.Thread):
     def __init__(self, num_logs, batch_size, time_in_minutes):
         super().__init__()
