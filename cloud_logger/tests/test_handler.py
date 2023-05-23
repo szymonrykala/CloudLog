2d52fb24cd4afa7b621e56706860f4cef6797479
BlayeeR <jakole004@utp.edu.pl>
2023-05-23 23:29:08 +0200

    Add global logger tests

--- cloud_logger/tests/test_handler.py
+++ cloud_logger/tests/test_handler.py
@@ -5,12 +5,13 @@ import pytest
 
 from cloudlog_commons import LogQueue
 
-
-@pytest.mark.parametrize("log_count", (
-    (0),
-    (5),
+@pytest.mark.parametrize("log_count, logger", (
+    (0, logging.getLogger('test_logger')),
+    (5, logging.getLogger('test_logger')),
+    (0, logging.getLogger()), # Global logger
+    (5, logging.getLogger()), # Global logger
 ))
-def test_local_handler(monkeypatch: pytest.MonkeyPatch, log_count: int):
+def test_handler(monkeypatch: pytest.MonkeyPatch, log_count: int, logger: logging.Logger):
     envs = {
         'CLOUDLOG_ENDPOINT': ''
     }
@@ -18,13 +19,17 @@ def test_local_handler(monkeypatch: pytest.MonkeyPatch, log_count: int):
 
     from cloudlogger import CloudLogHandler
 
-    logger = logging.getLogger('test_logger')
-    assert len(logger.handlers) == 0
+    # Filter out diffrent handlers and assert that there are not yet CloudLogHandlers
+    assert len([handler for handler in logger.handlers if isinstance(handler, CloudLogHandler)]) == 0
 
     clh = CloudLogHandler('test_app')
     logger.addHandler(clh)
     logger.setLevel(logging.INFO)
 
+    # Filter out diffrent handlers and assert that our handler is added
+    assert len([handler for handler in logger.handlers if isinstance(handler, CloudLogHandler)]) == 1
+
+    # Check if queue exists & is empty
     assert isinstance(clh.queue, LogQueue)
     assert len(clh.queue.pop(amount = 50)) == 0
 
