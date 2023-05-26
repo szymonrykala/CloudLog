add4c9f729c368211cd3ef137ab65a9b310ad33f
Szymon Rykala <szymonrykala@gmail.com>
2023-05-26 23:22:08 +0200

    improvements on queue and sender

--- cloudlog_commons/cloudlog_commons/__init__.py
+++ cloudlog_commons/cloudlog_commons/__init__.py
@@ -1,5 +1,3 @@
 from .log import DBLog, Log, OS, LogType
 from .database import DynamoTable, DynamoRequest
 from .logger import logger
-from .log_sender import LogSender
-from .log_queue import LogQueue
--- cloudlog_commons/cloudlog_commons/log_sender.py
+++ cloudlog_commons/cloudlog_commons/log_sender.py
@@ -1,54 +1,47 @@
-import os
 import threading
 import time
 
 import requests
 from requests_auth_aws_sigv4 import AWSSigV4
 
+from cloudlog_commons.log import Log
+
 from .log_queue import LogQueue
 
+
 class LogSender(threading.Thread):
-    # Static stuff
     queue: LogQueue = None
+    stopped: bool = False
+
+    def __init__(self, endpoint: str, batch_size: int, send_interval: int):
+        super().__init__()
+        self.endpoint = endpoint
+        self.batch_size = batch_size
+        self.send_interval = send_interval
+
     @classmethod
     def attach_queue(cls, queue: LogQueue):
-        cls.queue = queue
-
-
-
-    stopped: bool = False
-    endpoint: str
-    batch_size: int
-    send_interval: int
-
-    def __init__(
-            self,
-            endpoint=None,
-            batch_size=None,
-            send_interval=None
-        ):
-        super().__init__()
-        self.endpoint = endpoint or os.environ["CLOUDLOG_ENDPOINT"]
-        self.batch_size = batch_size or os.environ.get("CLOUDLOG_BATCH_SIZE", 25)
-        self.send_interval = send_interval or os.environ.get("CLOUDLOG_SEND_INTERVAL", 10)
+        if not cls.queue:
+            cls.queue = queue
 
     def stop(self):
         self.stopped = True
 
     def run(self):
-        while not self.stopped:
-            batch = LogSender.queue.pop(self.batch_size) if LogSender.queue else []
+        if self.queue:
+            while not self.stopped:
+                batch: tuple[Log] = self.queue.pop(self.batch_size)
 
-            if len(batch) > 0:
-                try:
-                    # Uses same env variables as AWS CLI
-                    auth = AWSSigV4('execute-api')
-                    response = requests.post(self.endpoint, json=batch, auth=auth)
-                    if response.status_code == 200:
-                        print(f"Sent batch with {len(batch)} logs")
-                    else:
-                        print(f"Failed to send logs with status code {response.status_code}")
-                except requests.exceptions.RequestException as e:
-                    print(f"Failed to send logs: {e}")
+                if len(batch) > 0:
+                    try:
+                        # Uses same env variables as AWS CLI
+                        auth = AWSSigV4("execute-api")
+                        response = requests.post(self.endpoint, json=batch, auth=auth)
+                        if response.status_code == 200:
+                            print(f"Sent batch with {len(batch)} logs")
+                        else:
+                            print(f"Failed to send logs: {response.status_code}")
+                    except requests.exceptions.RequestException as e:
+                        print(f"Failed to send logs: {e}")
 
-            time.sleep(self.send_interval)
+                time.sleep(self.send_interval)
